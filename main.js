baf5814f6b821abef4500a34ad92adaae5e12d76
jlozamarin <jenniferlozamarin@usf.edu>
2024-10-27 14:00:51 -0400

    Add event listener for product selection and quantity input

---
+++ main.js
@@ -0,0 +1,8 @@
+// Task 2: Add Event Listener for Product Selector
+
+let productSelector = document.getElementById('product-selector');
+let quantityInput = document.getElementById('quantity');
+let totalPriceElement = document.getElementById('total-price');
+let placeOrderButton = document.getElementById('place-order');
+let orderSummary = document.getElementById('order-summary');
+
